e919f6afc4bde045f7d1c36840ea1463042a20e9
hjsad1994 <trantai306@gmail.com>
2025-05-15 13:51:47 +0700

    first commit

---
+++ app/controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Default controller class
+ */
+class DefaultController
+{
+    /**
+     * Index action
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo "HELLO HUTECH";
+    }
+}
